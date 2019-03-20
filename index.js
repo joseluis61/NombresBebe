185baacae63b1b33a97d080714916d24d156bdc6
Lenovo-24-Mañana <versus.lola@gmail.com>
2019-03-20 10:25:10 +0100

    Base de datos de nombres creada

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ app.get("/",function(req,res){
   res.sendFile(__dirname+"/index.html");
 });
 
+app.post("/",function(req,res){
+  //Recojo los valores que envian desde el formulario.
+  console.log(req.body);
+  var sexo=req.body.sexo;
+  var apellidoPaterno=(req.body.apadre);
+  var apellidoMaterno=(req.body.amadre);
+
+  //Declaro dos arrays de nombres (masculinos y femeninos).
+  var nombresNene=["Andres","Julian","Pelayo","Martin","Rodrigo"];
+  var nombresNena=["Jimena","Fulgencia","Andrea","Petra","Lucia"];
+  //Escogemos aleatoriamente un nombre.
+  
+});
+
 //Arranca el servidor.
 app.listen(3000,function(){
   console.log("Servidor escuchando en el puerto 3000.");
